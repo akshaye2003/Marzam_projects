3f99bfc8288c0f5e76d00f216e20bb399a662df8
Akshaye <akshaye.anilkumar2022@vitstudent.ac.in>
2024-06-24 23:10:55 +0530

    Prepare for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { createBrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
-import Gallery from './Components/Gallery/Gallery.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <div>404 NOT FOUND</div>,
-  },
-  {
-    path: '/gallery',
-    element: <Gallery />
-  },
-]);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import Gallery from './Components/Gallery/Gallery.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <Router>
+      <Routes>
+         <Route path="/Marzam_projects/" element={<App />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
-)
+);
