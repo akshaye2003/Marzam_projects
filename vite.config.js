36130799fc028bd2b033fb46df31554a602da8fc
akshaye2003 <126176397+akshaye2003@users.noreply.github.com>
2024-08-06 19:36:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+var ghpages = require('gh-pages');
+var fs = require('fs');
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
 })
+
+
+fs.writeFile('src/CNAME', "marzamprojects.com", function(err) {});
+ghpages.publish('src', function(err) {});
